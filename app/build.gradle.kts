41e57ece5d288c908f2902a992718c1112834aad
yurii-android-dev <yurii.shcherbyna.dev@gmail.com>
2024-04-03 13:56:02 +0300

    update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -71,8 +71,11 @@ dependencies {
     debugImplementation("androidx.compose.ui:ui-test-manifest")
 
     // Hilt
-    implementation("com.google.dagger:hilt-android:2.48")
+    implementation("com.google.dagger:hilt-android:2.49")
     kapt("com.google.dagger:hilt-android-compiler:2.48")
+    implementation ("androidx.hilt:hilt-lifecycle-viewmodel:1.0.0-alpha03")
+    kapt ("androidx.hilt:hilt-compiler:1.2.0")
+    implementation ("androidx.hilt:hilt-navigation-compose:1.2.0")
 
     // Coil
     implementation("io.coil-kt:coil-compose:2.6.0")
