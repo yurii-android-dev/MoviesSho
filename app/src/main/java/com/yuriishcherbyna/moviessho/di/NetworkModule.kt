199efad5ed30cb73e99a921128341f76632b5adb
yurii-android-dev <yurii.shcherbyna.dev@gmail.com>
2024-04-05 11:01:36 +0300

    create NetworkModule

---
+++ app/src/main/java/com/yuriishcherbyna/moviessho/di/NetworkModule.kt
@@ -0,0 +1,32 @@
+package com.yuriishcherbyna.moviessho.di
+
+import com.yuriishcherbyna.moviessho.data.MoviesApi
+import com.yuriishcherbyna.moviessho.util.Constants.BASE_URL
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(MoshiConverterFactory.create())
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideMoviesApi(retrofit: Retrofit): MoviesApi {
+        return retrofit.create(MoviesApi::class.java)
+    }
+
+}
--- app/src/main/java/com/yuriishcherbyna/moviessho/util/Constants.kt
+++ app/src/main/java/com/yuriishcherbyna/moviessho/util/Constants.kt
@@ -3,5 +3,6 @@ package com.yuriishcherbyna.moviessho.util
 object Constants {
 
     const val POSTER_IMAGE_BASE_URL = "https://image.tmdb.org/t/p/w500"
+    const val BASE_URL = "https://api.themoviedb.org/3"
 
 }
