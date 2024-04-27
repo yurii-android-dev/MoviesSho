5777841c11e526e1e337b31be62f2bf25c9b1743
yurii-android-dev <yurii.shcherbyna.dev@gmail.com>
2024-04-27 12:20:02 +0300

    update Cast model class

--- app/src/main/java/com/yuriishcherbyna/moviessho/model/Cast.kt
+++ app/src/main/java/com/yuriishcherbyna/moviessho/model/Cast.kt
@@ -3,6 +3,7 @@ package com.yuriishcherbyna.moviessho.model
 import com.squareup.moshi.Json
 
 data class Cast(
+    val id: String,
     val name: String,
     @Json(name = "profile_path") val profilePath: String
 )
