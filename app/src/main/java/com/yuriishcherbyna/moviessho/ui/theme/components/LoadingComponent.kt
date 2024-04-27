75a2039634130fa6b84143beac6412bfb1f5350a
yurii-android-dev <yurii.shcherbyna.dev@gmail.com>
2024-04-27 12:20:28 +0300

    moved LoadingComponent to global components package

--- app/src/main/java/com/yuriishcherbyna/moviessho/ui/theme/components/LoadingComponent.kt
+++ app/src/main/java/com/yuriishcherbyna/moviessho/ui/theme/components/LoadingComponent.kt
@@ -1,4 +1,4 @@
-package com.yuriishcherbyna.moviessho.ui.theme.home.components
+package com.yuriishcherbyna.moviessho.ui.theme.components
 
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
