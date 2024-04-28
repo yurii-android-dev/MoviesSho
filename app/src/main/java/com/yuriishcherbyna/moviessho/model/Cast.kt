f14f617af43fd4b2205685a0f6f0b90cfc4713df
yurii-android-dev <yurii.shcherbyna.dev@gmail.com>
2024-04-28 15:18:26 +0300

    updated some model classes

--- app/src/main/java/com/yuriishcherbyna/moviessho/model/Cast.kt
+++ app/src/main/java/com/yuriishcherbyna/moviessho/model/Cast.kt
@@ -5,5 +5,5 @@ import com.squareup.moshi.Json
 data class Cast(
     val id: String,
     val name: String,
-    @Json(name = "profile_path") val profilePath: String
+    @Json(name = "profile_path") val profilePath: String?
 )
--- app/src/main/java/com/yuriishcherbyna/moviessho/model/MovieDetails.kt
+++ app/src/main/java/com/yuriishcherbyna/moviessho/model/MovieDetails.kt
@@ -3,12 +3,11 @@ package com.yuriishcherbyna.moviessho.model
 import com.squareup.moshi.Json
 
 data class MovieDetails(
-    @Json(name = "backdrop_path") val backdropPath: String,
+    @Json(name = "backdrop_path") val backdropPath: String?,
     val genres: List<Genre>,
     val id: Int,
     @Json(name = "original_language") val originalLanguage: String,
     val overview: String,
-    @Json(name = "poster_path") val posterPath: String,
     @Json(name = "release_date") val releaseDate: String,
     val runtime: Int,
     val title: String,
